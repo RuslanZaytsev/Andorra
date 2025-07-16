41d21bfe94b6b1192263ef1e015437fb434dda51
Ruzan <marktt@bk.ru>
2025-07-16 20:37:27 +0300

    попробовал написать контрролируемый инптут с лейблом и ошибкой

--- andorra/src/app/page.tsx
+++ andorra/src/app/page.tsx
@@ -1,14 +1,28 @@
+'use client'
+
+
 import {Input} from "@/shared/components/Input/Input";
 import styles from "./page.module.css";
+import {StateInput} from "@/shared/components/StateInput/StateInput";
+import {useState} from "react";
+import {TValue} from "@/shared/components/StateInput/types";
 
 export default function Home() {
+
+    const [state, setState] = useState<TValue>();
+
+    const hadleStateInputChange = (value: TValue) => {
+        setState(value)
+    }
+
     return (
         <div className={styles.page}>
             <Input type={'text'} placeholder={'текстовый инпут'}/>
             <Input type={'number'} placeholder={'введите число'}/>
             <Input type={'password'} passDifficultylevel={true}/>
+            <StateInput value={state!} label={"input"} placeholder={'плейсхолдер'} type={'text'} error={'error'} disabled={false}
+                        onChange={hadleStateInputChange}/>
         </div>
     )
         ;
 }
-//
---
+++ andorra/src/shared/components/StateInput/StateInput.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+
+import {ChangeEvent} from "react";
+import styles from './StateInput.module.scss'
+import {TValue} from "@/shared/components/StateInput/types";
+
+interface IInput {
+    label?: string,
+    type: 'text' | 'password' | 'email' | 'number',
+    placeholder?: string,
+    error?: string,
+    classname?: string,
+    disabled?: boolean;
+    value: TValue;
+    onChange: (value: TValue) => void;
+}
+
+export const StateInput = ({label, value, onChange, type, placeholder = '', error, disabled}: IInput) => {
+
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        onChange?.(e.target.value)
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            {label && <label>{label}</label>}
+            <input disabled={disabled} className={styles.input} type={type} placeholder={placeholder} value={value}
+                   onChange={handleChange}>
+            </input>
+            {error && <span className={styles.error}>{error}</span>}
+        </div>
+
+    )
+}
+
+// просто начал копать в сети как написать инпут на профессионально уровне и попробовал собрать свой с подсмотрами
+
+
