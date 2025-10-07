6a26e5ee207cc2430c7054630636c896e7e49901
Ruzan <marktt@bk.ru>
2025-10-07 18:58:06 +0300

    компонент свичт, базовое ДЖСдок описание

--- andorra/src/shared/components/Switch/Switch.tsx
+++ andorra/src/shared/components/Switch/Switch.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import {stylesUtils} from "@/shared/components/Switch/utils";
 
 import styles from './Switch.module.scss'
 
+/**
+ * @component
+ * @example
+ * // Basic usage without props
+ * <Switch />
+ */
+
 
 const Switch = () => {
-    return (
-        <div className={styles.switchWrapper}>
 
+    const [isOn, setIsOn] = useState<boolean>(false);
+
+    const handleChange = () => {
+        setIsOn(!isOn);
+    };
+
+    const trackStyles = stylesUtils(isOn, styles.track, styles.activeTrack)
+    const buttonStyles = stylesUtils(isOn, styles.button, styles.activeButton)
+
+    return (
+        <div onClick={handleChange} className={styles.switchWrapper}>
+            <div className={trackStyles}>
+                <div className={buttonStyles}></div>
+            </div>
         </div>
     );
 }
+
+Switch.displayName = 'Switch';
 
 export default Switch;
