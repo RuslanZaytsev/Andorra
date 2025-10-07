72b6db97c579534e2ff372ada57e7d70b9161677
Ruzan <marktt@bk.ru>
2025-10-07 18:01:15 +0300

    обертка свитча, тени, общий флексбокс

--- andorra/src/app/page.tsx
+++ andorra/src/app/page.tsx
@@ -3,12 +3,15 @@
 import Select from "@/shared/components/Select/Select";
 
 import styles from "./page.module.css";
+import Switch from '../shared/components/Switch/Switch';
+
 
 export default function Home() {
 
     return (
         <div className={styles.page}>
             <Select renderBody/>
+            <Switch/>
         </div>
     )
         ;
---
+++ andorra/src/shared/components/Switch/Switch.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './Switch.module.scss'
+
+
+const Switch = () => {
+    return (
+        <div className={styles.switchWrapper}>
+
+        </div>
+    );
+}
+
+export default Switch;
